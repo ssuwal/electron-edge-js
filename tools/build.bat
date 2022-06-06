a801cd37bae97187f5a152b388c6b09ca18fdd78
banzai_r2d2 <santosh@banzai.nyc>
2022-06-06 10:39:45 -0400

    node 16.13.2 and electron 18.2.3

--- tools/build.bat
+++ tools/build.bat
@@ -57,7 +57,9 @@ if "%3" equ "2.0.0" (
 ) else if "%3" equ "12.0.0" (
     SET target=14.16.0
 ) else if "%3" equ "13.0.0" (
-    SET target=14.16.0
+    SET target=14.17.0
+) else if "%3" equ "18.2.3" (
+    SET target=16.13.2
 ) else (
     echo edge-electron-js does not support Electron %3.
     exit /b -1
